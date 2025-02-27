19936f3455f4937afb133362f18c9d3f428ebf85
syahaeun <syaro_@naver.com>
2025-02-27 10:10:17 +0900

    [기능수정] GET방식에서 POST방식으로 수정

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
-
+from pydantic import BaseModel
 import chatbot_model
+
 app = FastAPI()
 
 cmodel = chatbot_model.ChatbotModel()
 
-@app.get("/chat")
-def chat(text: str = Query(), user: str = Query()):
-    response = cmodel.get_response(user, text)
-    return {"content" :response}
+class ChatRequest(BaseModel):
+    text: str
+    user: str
 
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = cmodel.get_response(request.user, request.text)
+    return {"content": response}
