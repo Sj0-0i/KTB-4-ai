937952dfd8c653ea24d725f3d41a438df0ba185a
Han Ju Kim <hjkim2902@gmail.com>
2025-02-27 11:42:10 +0900

    fix : main.py 추가, 엔드포인트 생성

---
+++ main.py
@@ -0,0 +1,42 @@
+import uvicorn
+from pydantic import BaseModel
+from fastapi import FastAPI
+from typing import List
+
+
+from app import chatbot_model
+from app.user_service import save_user_data
+
+cmodel = chatbot_model.ChatbotModel()
+
+app = FastAPI(title="Chatbot API")
+
+class ChatRequest(BaseModel):
+    message:str
+    userId:str
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = cmodel.get_response(request.userId, request.message)
+    return {"content" :response}
+
+class UserData(BaseModel):
+    id: str
+    age: int
+    like: List[str]
+
+@app.post("/user")
+def setUserData(request: UserData):
+    save_user_data(user_data=request)
+    return {"message": "User data received", "data": request}
+
+
+# 음성으로 전달하는거 만들예정 : Nick
+# @app.websocket("/voiceChat")
+# def chat():
+#     response = cmodel.get_response(request.userId, request.message)
+#     return {"content" :response}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
